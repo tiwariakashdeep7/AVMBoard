dba4b71584fe012b9a908fe2d6403f5e32d20fae
tiwariakashdeep7 <40921220+tiwariakashdeep7@users.noreply.github.com>
2025-04-24 13:25:32 +0530

    Update avmboard.py

--- avmboard.py
+++ avmboard.py
@@ -2,7 +2,7 @@
 
 import pandas as pd
 import numpy as np
-#import geopandas as gpd
+import geopandas as gpd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
